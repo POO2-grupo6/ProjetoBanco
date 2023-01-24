87c6b37f408448a7948e02505bf6654cc0d8ac5f
kleberton-santos <ti.klebersantos@gmail.com>
2023-01-24 13:19:11 -0300

    Mechendo com Enum

--- src/sinqia/client/FeesAndInvestments.java
+++ src/sinqia/client/FeesAndInvestments.java
@@ -1,9 +1,10 @@
 package sinqia.client;
 
-public enum FeesAndInvestments {
+import java.math.BigDecimal;
 
+public enum FeesAndInvestments {
     TRANSFER_INTEREST_RATE_JP,
     SAVINGS_INTEREST_RATE,
-    INVESTMENT_INTEREST_RATE
+    INVESTMENT_INTEREST_RATE;
 
 }
